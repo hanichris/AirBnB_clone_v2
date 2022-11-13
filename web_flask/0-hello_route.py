5332bb0be5e8d69aa6805e09e57ff79e05c34427
hanichris <hanichris71@gmail.com>
2022-11-13 20:32:26 +0000

    Moved configuration settings from `__init__.py` to `0-hello_route.py` file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""Flask web application."""
-from web_flask import app
+"""Instantiate an object of class Flask.
 
+The Flask instance is configured to listen on
+`0.0.0.0` on port 5000 to have the server available
+externally.
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config["SERVER_NAME"] = "0.0.0.0:5000"
 
 @app.route('/', strict_slashes=False)
 def hello():
